16ff0ea8b2f8c4e1016d73dae55c82c5d99871fe
beets-w <warrenbeets@gmail.com>
2020-04-02 08:19:05 +0100

    Initial commit

---
+++ markdpwn_git_test.R
@@ -0,0 +1,19 @@
+---
+title: "Markdown_Git_Test"
+author: "Warren Beets"
+date: "02 April 2020"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+
+My *first* version controlled project in RSTudio.
+
+Let's create a test plot
+
+```{r, test-plot}
+plot(cars)
+```
